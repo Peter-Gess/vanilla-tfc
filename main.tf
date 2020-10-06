788eed62af44f7e6c9f560dded2f817c260d6219
Peter-Gess <gesspeter@gmail.com>
2020-10-05 19:55:39 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,9 +6,21 @@ provider "aws" {
   region = var.aws_region
 }
 
+data "terraform_remote_state" "admin" {
+  backend = "remote"
+
+  config = {
+    organization = "vanilla-tfc"
+    workspaces = {
+      name = "vm-qa"
+    }
+  } 
+}
+
 resource "aws_instance" "ubuntu" {
   ami               = var.ami_id
   instance_type     = var.instance_type
+  subnet_id         = data.terraform_remote_state.vm-qa.outputs.subnet_id
 
   tags = {
     Name    = "pgess-tfe-demo"
