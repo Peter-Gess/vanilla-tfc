eb8a825cc9ed145877fd0be472642764faab746b
Peter-Gess <gesspeter@gmail.com>
2021-06-28 15:13:56 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,16 +5,3 @@ terraform {
 provider "aws" {
   region = var.aws_region
 }
-
-resource "aws_instance" "ubuntu" {
-  ami               = var.ami_id
-  instance_type     = var.instance_type
-
-  tags = {
-    Name    = "pgess-tfc-demo"
-    owner   = "Peter Gess"
-    region  = "Central Enterprise"
-    purpose = "TFC Demo"
-    ttl     = "2" 
-  }
-}
