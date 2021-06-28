7a5a249af7f1eadd56be50b39f4cd537636d44b8
Peter-Gess <gesspeter@gmail.com>
2021-06-28 15:23:08 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "ubuntu" {
-  count             = 1
+  count             = 0
   ami               = var.ami_id
   instance_type     = var.instance_type
 
